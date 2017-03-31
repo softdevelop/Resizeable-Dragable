2fb053342b172701ec850382b1383008863923e3
pacificsoftdevld <pacificsoftdev.ld@gmail.com>
2017-03-31 14:35:04 +0700

    Init

---
+++ README.md
@@ -0,0 +1,6 @@
+# Resizeable-Dragable
+
+Single page web application, there is vertical menu panel on the left, 200-250px wide. When clicking on its edge - It becomes like 50-70px. (slide collapse). 
+In main screen there is 3 widgets(windows) .
+Can move this windows and resize them. BUT! Windows can’t be placed on each other! They must be side-by-side windows all the time. 
+Their position and size will be saved after reload.
---
+++ js/data.js
@@ -0,0 +1,29 @@
+var databases = [
+    {
+        id: 0,
+        title:'Title 0',
+        img:'http://cdn.jssor.com/demos/img/landscape/01.jpg',
+        content:'Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.',
+        height: 404,
+        width: 335,
+        priority: 0
+    },
+    {
+        id: 1,
+        title:'Title 1',
+        img:'http://cdn.jssor.com/demos/img/landscape/02.jpg',
+        content:'Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.',
+        height: 404,
+        width: 335,
+        priority: 1
+    },
+    {
+        id: 2,
+        title:'Title 2',
+        img:'http://cdn.jssor.com/demos/img/landscape/03.jpg',
+        content:'Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.',
+        height: 404,
+        width: 335,
+        priority: 2
+    },
+]
