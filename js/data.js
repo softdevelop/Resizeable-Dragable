5b7cc3c8e3a21fd87d539dc63cacc9d3fa2e196d
pacificsoftdevld <pacificsoftdev.ld@gmail.com>
2017-07-14 16:01:13 +0700

    Update code

--- js/data.js
+++ js/data.js
@@ -2,7 +2,7 @@ var databases = [
     {
         id: 0,
         title:'Title 0',
-        img:'http://cdn.jssor.com/demos/img/landscape/01.jpg',
+        img:'dist/img/01.jpg',
         content:'Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.',
         height: 404,
         width: 335,
@@ -11,7 +11,7 @@ var databases = [
     {
         id: 1,
         title:'Title 1',
-        img:'http://cdn.jssor.com/demos/img/landscape/02.jpg',
+        img:'dist/img/02.jpg',
         content:'Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.',
         height: 404,
         width: 335,
@@ -20,7 +20,7 @@ var databases = [
     {
         id: 2,
         title:'Title 2',
-        img:'http://cdn.jssor.com/demos/img/landscape/03.jpg',
+        img:'dist/img/03.jpg',
         content:'Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.',
         height: 404,
         width: 335,
